6bbe914bc14a7dbcceebcaf23ec54b87295752ef
Gabriele Bovolenta <gabriele.bovolenta@edu.itspiemonte.it>
2022-06-16 17:52:14 +0200

    Modified setting page, added validation

--- scripts/setting.js
+++ scripts/setting.js
@@ -4,8 +4,6 @@
  * validate-form
  */
 
-import { config as cnf } from "./config.mjs";
-
 // SETTING FORM
 let setting = document.getElementById("target");
 let form = document.getElementById("hidden-setting");
@@ -15,13 +13,23 @@ setting.addEventListener("click", function () {
 });
 
 
-const kswee = document.getElementById('weeks-setting');
+const weeks = document.getElementById('weeks-setting');
 const weeklyProduction = document.getElementById('weekly-production-setting');
+const weeklyDuration = document.getElementById('weekly-duration-setting');
+const checkThreshold= document.getElementById('check-threshold-setting');
+const dateOffset = document.getElementById('date-offset-setting');
+
 
 function validation() {
+    cancel();
+
     controlNumber(weeks, 1, 10, 1);
     controlNumber(weeklyProduction, 1, 5, 2);
-}
+    controlNumber(weeklyDuration, 1, 10, 3);
+    controlNumber(checkThreshold, 1, 10, 4);
+    controlNumber(dateOffset, 1, 10, 6);
+
+};
 
 function controlNumber(element, minore, maggiore, child) {
     if((/\D|^0/gi.test(element.value))) {
@@ -33,9 +41,18 @@ function controlNumber(element, minore, maggiore, child) {
     else if (element.value > maggiore) {
         document.querySelector(`#form div:nth-of-type(${child}) h5`).textContent = 'Devi inserire un numero minore di ' + maggiore;
     }
-    else {
-        alert('your imput is ok');
-    }
+}
+
+function cancel () {
+    let title = document.querySelectorAll('h5');
+    title.forEach(element => {
+        element.textContent = "";
+    });
+}
+
+function dateControll(element, mag, min, child) {
+    let date = element.date()
+    
 }
 
 const button = document.getElementById('submit');
