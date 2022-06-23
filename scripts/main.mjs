337665f5c4b6ad5840bb7e254a5e831f94faecb9
Raul Bercea <raulbercea@gmail.com>
2022-06-23 21:26:56 +0200

    added comments

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -46,64 +46,92 @@ let init = () => {
     // Add new items
     items.push(...fn.generateItems(cnf.newItemsPerWeek, startConfig));
 
+    // add unfiltered items to the weeks array
     weeks.push(items);
-    console.log(currentWeek);
+    // printing the unfiltered items on the table in the dom
     fng.printItems(mainTable, weeks[currentWeek]);
 
+    // filtering items
     items = items.filter(fn.checkItem);
+
+    // adding the filtered items to the filter array
     filteredWeek.push(items);
+
+    // printing the filtered items to the filtered table in the dom
     fng.printItems(tableFiltered, filteredWeek[currentWeek]);
 
     // Add days to the current date
     currentDate = fn.addDays(currentDate, cnf.daysInWeek);
 
+    // checking the status of the item and changing it if necessary
     items.forEach((item) => {
       fn.updateChecks(item);
       fn.updateState(item, currentDate, cnf.shelfLife);
     });
   };
 
+  // first creation of the table
   addTableOfItems();
 
+  /**
+   * function that toggles the left and right buttons based on the position of
+   * the array
+   */
   let toggleButton = () => {
+    // hide the back button when the array is at 0
     if (currentWeek === 0) {
       global.backButton.style.visibility = "hidden";
     }
+    // hiding the forward button when the array reaches the max
     else if (currentWeek === runtime) {
       global.forwardButton.style.visibility = "hidden";
     }
+    // when the array is between 0 and the max runtime both buttons are visible
     else if (currentWeek > 0 && currentWeek < runtime) {
       global.backButton.style.visibility = "visible";
       global.forwardButton.style.visibility = "visible";
     }
   }
 
+  /**
+   * Function that prints the next or previous week of items
+   * @param {String} direction - weather to print the next or previous week
+   */
   let tableMove = (direction) => {
-
+    // check if the maximum number of weeks has been printed
     if (currentWeek === runtime) {
       hasReachedLimit = true;
     }
-
+    // printing the next week if there is still weeks to be printed
     if (direction === "++" && !global.hasReachedLimit) {
+      // remove the content of the table
       fng.clearTable(mainTable);
       fng.clearTable(tableFiltered);
+      // increase the week counter
       currentWeek++;
+      // print the week
       addTableOfItems();
-    } else if (direction === "--" && currentWeek > 0) {
+    }
+    // if above the starting week print the previous week
+    else if (direction === "--" && currentWeek > 0) {
+      // clear the table
       fng.clearTable(mainTable);
       fng.clearTable(tableFiltered);
+      // decrease the week count
       currentWeek--;
+      // print the week
       addTableOfItems();
     }
   };
 
-  global.backButton.addEventListener("click", () => {
-    tableMove("--");
-    toggleButton();
+  // event listener for the click of the back button
+  global.backButton.addEventListener("mousedown", () => {
+    tableMove("--");// make the week go back
+    toggleButton();// toggle the button if at the minimum
   });
   global.forwardButton.addEventListener("mousedown", () => {
-    tableMove("++");
-    toggleButton();
+    tableMove("++");// make the week go back
+    toggleButton();// toggle the button if at the minimum
   });
 };
 
